c8322645f569a7eb5aa85c3063288ae399b55b1b
Patrick Pehrs <patrick.pehrs@sportalliance.com>
2023-01-04 17:03:48 +0100

    4

---
+++ build/precache-manifest.762ca8089e54584df444ce8bfd551752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39d75e6854a8703c1b3f",
+    "url": "/static/css/main.9a4c05a2.chunk.css"
+  },
+  {
+    "revision": "39d75e6854a8703c1b3f",
+    "url": "/static/js/main.07926d69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2aa13728dfc09ffce35b",
+    "url": "/static/js/2.e2c0aa4f.chunk.js"
+  },
+  {
+    "revision": "97e4e3784f532025b72f56189a2d9599",
+    "url": "/index.html"
+  }
+];
