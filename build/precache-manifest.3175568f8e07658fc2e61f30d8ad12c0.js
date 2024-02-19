42d62035d096949276c2bd37ed1ac615f89259a8
Patrick1900 <patrick.pehrs@sportalliance.com>
2024-02-19 17:18:42 +0100

    wtf 2

---
+++ build/precache-manifest.3175568f8e07658fc2e61f30d8ad12c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ca248fbd92af5b43ded",
+    "url": "/static/css/main.e0b6f337.chunk.css"
+  },
+  {
+    "revision": "3ca248fbd92af5b43ded",
+    "url": "/static/js/main.75e35cf8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "103fc95f47bd9e3b70c3",
+    "url": "/static/js/2.7f6385bd.chunk.js"
+  },
+  {
+    "revision": "1a986979f6c4faa52acbf3ef2a34d95c",
+    "url": "/index.html"
+  }
+];
